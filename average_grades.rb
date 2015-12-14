35e671b8936b3f204d164d864b8e3de72bc1af6f
philiptang42 <philiptang42@gmail.com>
2015-12-14 00:23:23 -0500

    part 1 solution

--- average_grades.rb
+++ average_grades.rb
@@ -1,3 +1,12 @@
-def average(grades)
+Jane = [98, 95, 88, 97, 74]
 
+def average(grades)
+  sum = 0
+  grades.each do |grade|
+    sum += grade
+  end
+
+  sum / grades.size.to_f
 end
+
+puts average(Jane)
