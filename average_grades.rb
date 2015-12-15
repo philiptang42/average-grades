b72004fe888350b60193be0cac455a70a11d017a
philiptang42 <philiptang42@gmail.com>
2015-12-14 23:38:02 -0500

    added all 3 grade lists

--- average_grades.rb
+++ average_grades.rb
@@ -1,4 +1,6 @@
 Jane = [98, 95, 88, 97, 74]
+Samantha = [85, 93, 98, 88, 49]
+Matt = [87, 93, 89, 97, 65]
 
 def average(grades)
   sum = 0
@@ -10,3 +12,5 @@ def average(grades)
 end
 
 puts average(Jane)
+puts average(Samantha)
+puts average(Matt)
